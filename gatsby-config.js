d9aa87c7391f70c9aea7523411a53d0f4a25682f
Sam Rizvi <srizvi@users.noreply.github.com>
2021-07-14 03:50:18 -0700

    Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Arshan - Superhero Software Engineer`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Arshan Mufti`,
+        short_name: `Arshan`,
+        description: `I'm going to be the world's greatest software engineer.`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
